8b12cab04e044da779d6d3b7d9933b9a8fdd9e8e
Bob Zoller <bob@zoller.us>
2018-10-01 14:27:02 -1000

    use official go-pagerduty client
    removes uses of the v1 PagerDuty API that is being decommissioned.
    https://v2.developer.pagerduty.com/docs/v1-rest-api-decommissioning-faq

--- pagerduty/schedules.go
+++ pagerduty/schedules.go
@@ -1,99 +1,109 @@
 package pagerduty
 
-import(
-  "fmt"
-  "time"
-  log "github.com/Sirupsen/logrus"
+import (
+	"time"
+
+	"github.com/PagerDuty/go-pagerduty"
+	log "github.com/Sirupsen/logrus"
 )
 
 type Schedules []Schedule
 
-type Schedule struct{
-  Id              string
-  Name            string
-  Timezone        string    `json:"time_zone"`
-  CurrentPeriod   *CallPeriod
-  NextPeriod      *CallPeriod
+type Schedule struct {
+	Id            string
+	Name          string
+	Timezone      string `json:"time_zone"`
+	CurrentPeriod *CallPeriod
+	NextPeriod    *CallPeriod
 }
 
-type CallPeriod struct{
-  Start     time.Time
-  User      string
+type CallPeriod struct {
+	Start time.Time
+	User  string
 }
 
 // Fetch the main schedule list then the details about specific schedules
-func (a *Api) Schedules() (Schedules, error){
-  var schdList Schedules
+func (a *Api) Schedules() (Schedules, error) {
+	var schdList Schedules
 
-  err := a.requestThing("schedules", &schdList)
-  if err != nil {
-    return schdList, err
-  }
+	res, err := a.client.ListSchedules(pagerduty.ListSchedulesOptions{})
+	if err != nil {
+		return schdList, err
+	}
 
-  var today string = time.Now().UTC().Format("2006-01-02")
-  var nextWeek string = time.Now().UTC().Add(time.Hour*24*7).Format("2006-01-02")
+	var today string = time.Now().UTC().Format("2006-01-02")
+	var nextWeek string = time.Now().UTC().Add(time.Hour * 24 * 7).Format("2006-01-02")
 
-  for i,_ := range schdList{
-    schd := &schdList[i]
-    rsp, err := a.request(fmt.Sprintf("schedules/%s/entries?since=%s&until=%s&time_zone=%s&overflow=true", schd.Id, today, nextWeek, a.timezone))
-    if err != nil {
-      return schdList, err
-    }
+	for _, bareSchedule := range res.Schedules {
+		schd := &Schedule{}
 
-    var schdInfo struct{
-      Entries     []struct{
-        User        struct{
-          Id          string
-        }
-        Start       time.Time
-        End         time.Time
-      }
-    }
+		schd.Id = bareSchedule.ID
+		schd.Name = bareSchedule.Name
+		schd.Timezone = bareSchedule.TimeZone
 
-    rsp.Unmarshal(&schdInfo)
+		res, err := a.client.GetSchedule(bareSchedule.ID, pagerduty.GetScheduleOptions{
+			TimeZone: a.timezone,
+			Since:    today,
+			Until:    nextWeek,
+		})
+		if err != nil {
+			return schdList, err
+		}
 
-    var activeEntries int
-    for _,se := range schdInfo.Entries{
-      if se.Start.Before(time.Now().UTC()) && se.End.After(time.Now().UTC()){
-        if activeEntries == 0{
-          p := CallPeriod{}
-          p.Start = se.Start
-          p.User = se.User.Id
-          schd.CurrentPeriod = &p
-        }
-        activeEntries += 1
-      }
+		var activeEntries int
+		for _, se := range res.FinalSchedule.RenderedScheduleEntries {
+			start, err := time.Parse(time.RFC3339Nano, se.Start)
+			if err != nil {
+				return schdList, err
+			}
 
-      if se.Start.After(time.Now().UTC()) && (schd.NextPeriod == nil || se.Start.Before(schd.NextPeriod.Start)){
-        p := CallPeriod{}
-        p.Start = se.Start
-        p.User = se.User.Id
-        schd.NextPeriod = &p
-      }
-    }
+			end, err := time.Parse(time.RFC3339Nano, se.End)
+			if err != nil {
+				return schdList, err
+			}
 
-    lf := log.Fields{
-      "id": schd.Id,
-    }
+			if start.Before(time.Now().UTC()) && end.After(time.Now().UTC()) {
+				if activeEntries == 0 {
+					p := CallPeriod{}
+					p.Start = start
+					p.User = se.User.ID
+					schd.CurrentPeriod = &p
+				}
+				activeEntries += 1
+			}
 
-    if schd.CurrentPeriod == nil{
-      log.WithFields(lf).Warning("No active current period for schedule")
-    } else {
-      lf["currentCall"] = schd.CurrentPeriod.User
-    }
+			if start.After(time.Now().UTC()) && (schd.NextPeriod == nil || start.Before(schd.NextPeriod.Start)) {
+				p := CallPeriod{}
+				p.Start = start
+				p.User = se.User.ID
+				schd.NextPeriod = &p
+			}
 
-    if schd.NextPeriod == nil{
-      log.WithFields(lf).Warning("No active next period for schedule")
-    } else {
-      lf["nextCall"] = schd.NextPeriod.User
-      lf["changeover"] = schd.NextPeriod.Start
-    }
+			schdList = append(schdList, *schd)
+		}
 
-    if activeEntries > 1{
-      log.WithFields(lf).Warning("Multiple active schedules")
-    }
-    log.WithFields(lf).Debug("Got schedule entries")
-  }
+		lf := log.Fields{
+			"id": schd.Id,
+		}
 
-  return schdList, nil
+		if schd.CurrentPeriod == nil {
+			log.WithFields(lf).Warning("No active current period for schedule")
+		} else {
+			lf["currentCall"] = schd.CurrentPeriod.User
+		}
+
+		if schd.NextPeriod == nil {
+			log.WithFields(lf).Warning("No active next period for schedule")
+		} else {
+			lf["nextCall"] = schd.NextPeriod.User
+			lf["changeover"] = schd.NextPeriod.Start
+		}
+
+		if activeEntries > 1 {
+			log.WithFields(lf).Warning("Multiple active schedules")
+		}
+		log.WithFields(lf).Debug("Got schedule entries")
+	}
+
+	return schdList, nil
 }
